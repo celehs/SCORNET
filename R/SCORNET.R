3e03f74d76489305b2f836a3d78f0d9e84b2affb
Yuri <yuri_ahuja@hms.harvard.edu>
2020-10-12 11:26:59 -0400

    Rewrote kernel smoothing of C|Z0 hazard in estimate.h in C++. Used outer function in lieu of nested sapply's where applicable. Included commenting and rewrote code to be more readable wherever possible.

--- R/SCORNET.R
+++ R/SCORNET.R
@@ -9,7 +9,7 @@ NULL
 # survival curve estimator that boosts efficiency over existing non-parametric estimators
 # by (1) utilizing unlabeled patients in a semi-supervised fashion, and (2) leveraging
 # information-dense engineered EHR features to maximize unlabeled set imputation precision
-# See Ahuja et al. (2020) Submitted to ... for details
+# See Ahuja et al. (2020) BioArxiv for details
 
 expit <- function(x){
   ifelse(x>=100,1,exp(x)/(1+exp(x)))
@@ -19,7 +19,7 @@ logit <- function(x){
   log(x/(1-x))
 }
 
-# Default kernel function = standard normal PDF
+# Default kernel function: standard normal PDF
 Knorm <- function(t0,t,b=1){
   dnorm(abs(t-t0),sd=b)
 }
@@ -27,24 +27,26 @@ Knorm <- function(t0,t,b=1){
 `%dopar%` <- foreach::`%dopar%`
 
 # Kernel-Smoothed Cox/Breslow estimator of C|Z0
-g <- function(C,Z=NULL,K=Knorm,b=1){
+estimate.h <- function(C,Z=NULL,b=NULL){
   N <- length(C)
   if (is.null(Z)){Z <- matrix(1,nrow=length(C),ncol=1)}
+  if (is.null(b)){b <- N^(-1/4) * min(sd(C), (quantile(C,0.75)-quantile(C,0.25))/1.34)}
+  
   beta_C.Z <- survival::coxph(survival::Surv(C)~Z)$coefficients; beta_C.Z[is.na(beta_C.Z)] <- 0
-  denom <- sapply(1:N,function(j){sum(exp(as.matrix(Z[C >= C[j],]) %*% beta_C.Z))})
+  
+  ord <- order(C); ordInv <- order(ord)
+  Z <- as.matrix(Z[ord,]); C <- C[ord]
+  denom <- sapply(1:N,function(i){sum(exp(as.matrix(Z[i:N,]) %*% beta_C.Z))})
   hC0 <- 1 / denom
-  hC0k <- foreach::foreach(c1=C, .combine=c, .export='K') %dopar% {
-    hC0 %*% sapply(C,function(c2){K(c1,c2,b)})
-  }
-  HC0 <- foreach::foreach(c=C, .combine=c, .export='trapz') %dopar% {
-    lt <- which(C <= c)
-    lt <- lt[order(C[lt])]
-    pracma::trapz(C[lt],hC0k[lt])
+  hC0k <- c(kernelSmoothen(hC0,C,b))
+  
+  HC0 <- foreach::foreach(i=1:N, .combine=c, .export='trapz') %dopar% {
+    pracma::trapz(C[1:i],hC0k[1:i])
   }
   SC0 <- exp(-HC0)
   h <- exp(Z %*% beta_C.Z)
   
-  as.vector(h * hC0k * SC0^h)
+  as.vector(h * hC0k * SC0^h)[ordInv]
 }
 
 #' SCORNET Estimator
@@ -60,11 +62,11 @@ g <- function(C,Z=NULL,K=Knorm,b=1){
 #' @param Zehr.UL Unlabeled set EHR-derived feature matrix
 #' @param K Kernel function (defaults to standard normal) 
 #' @param b bandwidth (default set heuristically)
-#' @param ghat N^1/4-consistent pdf estimator of C|Z0 (defaults to Kernel-Smoothed Cox/Breslow estimator)
+#' @param h_hat N^1/4-consistent pdf estimator of C|Z0 (defaults to Kernel-Smoothed Cox/Breslow estimator)
 #' @return S_hat: Survival function estimates at times t0.all; StdErrs: Asymptotically consistent standard error estimates corresponding to S_hat
 #' @export
 scornet <- function(Delta, C, t0.all, C.UL = NULL, filter = NULL, filter.UL = NULL, Z0 = NULL, Z0.UL = NULL,
-                     Zehr = NULL, Zehr.UL = NULL, K = Knorm, b = NULL, ghat = NULL) {
+                     Zehr = NULL, Zehr.UL = NULL, K = Knorm, b = NULL, h_hat = NULL) {
   Ctot <- c(C,C.UL)
   N <- length(C)
   Ntot <- length(Ctot)
@@ -86,29 +88,32 @@ scornet <- function(Delta, C, t0.all, C.UL = NULL, filter = NULL, filter.UL = NU
   }
   nu <- Ntot^(-1/4) * min(sd(Ctot), (quantile(Ctot,0.75)-quantile(Ctot,0.25))/1.34)
   
-  if (is.null(ghat)){
-    ghat <- g(Ctot,Z0tot,K,nu)
+  
+  # STEP 1A: Estimate density h(C|Z0)
+  
+  if (is.null(h_hat)){
+    h_hat <- estimate.h(Ctot,Z0tot,nu)
   }
   
-  # Estimate S_T
-  Kmat <- sapply(t0.all,function(t){
-    sapply(Ctot,function(c){
-      K(c,t,b)
-    })
-  })
+  
+  # STEP 1B: Estimate probability P(T<=t|Z,Z0,C)
+  
+  Kmat <- outer(Ctot,t0.all,function(x,y){K(x,y,b)})
   beta_T.Z <- sapply(1:length(t0.all),function(i){
     glm(Delta~cbind(Z0,Zehr), family=binomial, weights=filter*Kmat[1:N,i])$coef
   })
   beta_T.Z[is.na(beta_T.Z)] <- 0
   
-  Fi_hat <- sapply(1:length(t0.all),function(i){
-    t <- t0.all[i]
-    expit(cbind(1,Z0tot,Zehrtot) %*% beta_T.Z[,i])
-  })
-  Fi_hat[!filtertot,] <- 0
-  S_hat <- 1 - (colSums(Fi_hat*Kmat/ghat) / colSums(Kmat/ghat))
   
-  # Estimate Std Errs
+  # STEP 2: Estimate marginal survival function S_T(t)
+  
+  Fi_hat <- expit(cbind(1,Z0tot,Zehrtot) %*% beta_T.Z)
+  Fi_hat[!filtertot,] <- 0
+  S_hat <- 1 - (colSums(Fi_hat*Kmat/h_hat) / colSums(Kmat/h_hat))
+  
+  
+  # Estimate standard errors for S_T(t)
+  
   tau_sq <- 1 / (2*sqrt(pi))
   StdErrs <- sapply(1:length(t0.all),function(i){
     tryCatch({
@@ -126,7 +131,7 @@ scornet <- function(Delta, C, t0.all, C.UL = NULL, filter = NULL, filter.UL = NU
         ABA <- solve(At) / (b * sum(Kmat[1:N,i]) / tau_sq)
         
         gTWprime <- Fi_hat[filtertot,i] * (1-Fi_hat[filtertot,i])
-        P <- t(W) %*% (Kmat[filtertot,i]*gTWprime/ghat[filtertot]) / sum(Kmat[,i]/ghat)
+        P <- t(W) %*% (Kmat[filtertot,i]*gTWprime/h_hat[filtertot]) / sum(Kmat[,i]/h_hat)
         ABAP <- c(t(P) %*% ABA %*% P)
         
         sqrt(ABAP)
